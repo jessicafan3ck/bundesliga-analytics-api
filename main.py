a06eb6d5d1658c13595ff584d0357f4b0490660e
jessicafan3ck <99548796+jessicafan3ck@users.noreply.github.com>
2025-07-15 11:00:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -248,5 +248,5 @@ def clustering():
 # -------------------------
 # Run locally for testing
 # -------------------------
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+#if __name__ == "__main__":
+    #uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
